cadf57f7a95d78fbfb8e12cf4484462cf6fa2aca
Lonnie-PublicSq <105244916+Lonnie-PublicSq@users.noreply.github.com>
2022-05-17 15:39:52 -0700

    Update streamlit_app.py
    Add list of selected fruits into "fruits_selected" variable, pull data from full table named "fruits_to_show", display "fruits_to_show" data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,6 @@ streamlit.header('🍌🍊 Create Your Own Smoothie 🥝🍑')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(my_fruit_list)
