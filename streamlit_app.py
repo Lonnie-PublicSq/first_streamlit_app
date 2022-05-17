51e758c3abdfd0040a8ba574195fd5090cc0cc22
Lonnie-PublicSq <105244916+Lonnie-PublicSq@users.noreply.github.com>
2022-05-17 14:56:57 -0700

    Update streamlit_app.py
    Fixed "Kale" Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥤🥬 Kate, Spinach & Rocket Smoothie')
+streamlit.text('🥤🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚🐔 Hard-Boiled Free-Range Egg')
